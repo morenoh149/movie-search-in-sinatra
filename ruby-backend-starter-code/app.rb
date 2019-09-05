f2afa00a62c712099ea263cdcdbffb85ab443fa3
Harry Moreno <morenoh149@gmail.com>
2019-09-05 01:13:20 -0400

    Fixup backend
    * add rerun to speed up api development
    * init data.json
    * use erb templates

--- ruby-backend-starter-code/app.rb
+++ ruby-backend-starter-code/app.rb
@@ -1,20 +1,27 @@
 require 'sinatra'
 
-get '/'
-  File.read('index.html')
+get '/' do
+  erb :index
 end
 
-get 'favorites' do
+# responds with json data for the favorited movies
+get '/favorites' do
   response.header['Content-Type'] = 'application/json'
   File.read('data.json')
 end
 
-get '/favorites' do
+# assumes data.json is a json object that is a single array.
+# like, [ {}, {}, {} ]
+# if query params are provided we persist the new entry
+# for more ideas study https://stackoverflow.com/questions/23292530/appending-the-data-in-json-file-using-ruby/23293518#23293518
+post '/favorites' do
   file = JSON.parse(File.read('data.json'))
+  # matches "POST /favorites?name=foo&oid=bar"
   unless params[:name] && params[:oid]
     return 'Invalid Request'
+  end
   movie = { name: params[:name], oid: params[:oid] }
   file << movie
-  File.write('data.json',JSON.pretty_generate(file))
+  File.write('data.json', JSON.pretty_generate(file))
   movie.to_json
 end
